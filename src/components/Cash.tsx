ec3350cfe54736c3af1de72484d7735e87f44b3f
anyuka007 <anna.popova.de@gmail.com>
2025-04-10 11:37:42 +0200

    Update Cash component to reset amount based on selectedPlayer and improve input handling

--- src/components/Cash.tsx
+++ src/components/Cash.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Player } from "../App";
 
 type CashProps = {
@@ -10,6 +10,16 @@ const OkButtonStyle = "w-10 border p-2 rounded-lg text-center cursor-pointer tra
 
 const Cash = ({ selectedPlayer, updateCash }: CashProps) => {
     const [amount, setAmount] = useState<number>(0);
+
+    useEffect(() => {
+        if (selectedPlayer) {
+          setAmount(selectedPlayer.score); 
+        } else {
+          setAmount(0); // Setze `amount` auf 0, wenn kein Spieler ausgewählt ist
+        }
+      }, [selectedPlayer]);
+    
+    
     const handleCashChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setAmount(Number(e.target.value));
     }
@@ -22,9 +32,11 @@ const Cash = ({ selectedPlayer, updateCash }: CashProps) => {
     }
 
 return (
-    <div className='flex justify-start items-center gap-4 w-3/5 mx-auto mt-5'>
+    <div className='flex justify-start items-center gap-4 w-3/5 mx-auto mt-7'>
         <p>Cash</p>
-        <input className="border p-2 rounded-lg" type="number" onChange={handleCashChange} value={selectedPlayer?.score}/>
+        <input className="border p-2 rounded-lg" type="number" 
+        onChange={handleCashChange} 
+        value={amount === 0 ? "" : amount}/>
         <button className={OkButtonStyle} onClick={handleOkClick}>Ok</button>
     </div>
 );
